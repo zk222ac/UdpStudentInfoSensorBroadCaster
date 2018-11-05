b820753482e6dca54224202f58cdbda9d34170ab
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-11-05 21:00:47 +0100

    Fake student Info UDP Sensor

--- UdpStudentInfoSensorBroadCaster/Program.cs
+++ UdpStudentInfoSensorBroadCaster/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UdpStudentInfoSensorBroadCaster
 {
+    /*
+ * Fake student Info UDP Sensor
+ *
+ * Author Zuhair haroon Khan, ZIBAT Computer Science
+ * Version 1.0. 2018.11.05
+ * Copyright 2018 by Zuhair haroon Khan * 
+ * All rights reserved
+   
+ */
     class Program
     {
         static void Main(string[] args)
         {
+            UdpClient udpSender = new UdpClient(0) {EnableBroadcast = true};
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Broadcast, 7000);
+            Console.WriteLine("Broadcast ready. Get started Press Enter");
+            Console.ReadLine();
+            while (true)
+            {
+                int i = 0;
+                RandomNumberNameClass _rand = new RandomNumberNameClass();
+                string fullName = RandomNumberNameClass.GetName(i);
+                int age = _rand.GetAge();
+                int Id = _rand.GetId();
+                DateTime currentTime = DateTime.Now;
+                string timeTxt = "Time: " + currentTime + " \r\n";
+                //string sensorData = sensorLocation + timeTxt + data;
+                string sensorData = $" Name:{fullName}-\r\n Age:{age}-\r\n Id:{Id}-\r\n CurrentTime:{currentTime} -\r\n\r\n";
+
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(sensorData);
+
+                try
+                {
+                    udpSender.Send(sendBytes, sendBytes.Length, endPoint); 
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                i++;
+                Console.WriteLine("Name:" + fullName);
+                Console.WriteLine("Age:" + age);
+                Console.WriteLine("Id:" + Id);
+                Console.WriteLine("DateTime:" + currentTime);
+                Console.WriteLine("Broadcasting data on Port no: 7000" );
+                //Console.WriteLine(sensorData);
+                Console.WriteLine("-----------------------------------------");
+                Thread.Sleep(1000);
+            }
+
+
         }
     }
 }
---
+++ UdpStudentInfoSensorBroadCaster/RandomNumberNameClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RandomNameGenerator;
+
+namespace UdpStudentInfoSensorBroadCaster
+{
+    class RandomNumberNameClass
+    {
+        private Random _randomId;
+        private Random _randomAge;
+        public RandomNumberNameClass()
+        {
+            _randomId = new Random();
+            _randomAge = new Random();
+        }
+        public int GetId()
+        {
+            return _randomId.Next(100);
+        }
+        public int GetAge()
+        {
+            return _randomAge.Next(20, 40);
+        }
+        public static string GetName(int i)
+        {
+            if (i % 2 == 0)
+            {
+                return NameGenerator.Generate(Gender.Male);
+            }
+            else
+            {
+                return NameGenerator.Generate(Gender.Female);
+            }
+
+        }
+    }
+}
